24862a052399f350080f27e5dd8629d4e75b2ed5
Thuson <thusonsm@gmail.com>
2025-06-06 12:58:28 -0400

    MR-28 Fix
    Applied CSS Naming Convention

--- js/contact.js
+++ js/contact.js
@@ -1,20 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const customLine = document.querySelector(".custom-line");
+  const customLine = document.querySelector(".mrn-c-contact-line");
   if (customLine) {
     setTimeout(() => {
       customLine.classList.add("visible");
     }, 200);
   }
+
   const elementsToFade = [
-    document.querySelector("#contact h2"),
-    document.querySelector("#contact p"),
-    document.querySelector("#contact .col-md-5"),
-    document.querySelector("#contact .col-md-7")
+    document.querySelector(".mrn-c-contact-section h2"),
+    document.querySelector(".mrn-c-contact-section p"),
+    document.querySelector(".mrn-l-contact-col"),
+    document.querySelector(".mrn-c-contact-section .col-md-7")
   ];
 
   elementsToFade.forEach((el, i) => {
     if (el) {
-      el.classList.add("fade-in");
+      el.classList.add("mrn-l-fade-in");
       setTimeout(() => {
         el.classList.add("visible");
       }, 300 * i);
