0aa9af4c86c8f1302a4e6eea85e7a7454e9fdc39
Dickson Lee <dicksonT_lee@hotmail.com>
2025-06-14 12:56:53 -0400

    feature/MR-37: Streamline SQL queries injection process + create new GET endpoints

--- README.md
+++ README.md
@@ -5,5 +5,9 @@
 1. Download NodeJS https://nodejs.org/en
 2. Check if NodeJS is properly installed on your computer with `node -v` within your terminal. #output: node's version
 3. Execute the cmd `npm install or npm ` within your terminal at the root level of this project to install the dependencies and node_modules
-4. Create the database, run sql queries and be sure to use your credentials in `index.js`
-5. Execute the cmd ``nodemon` to start the server.
+4. Create the database, run sql queries and be sure to use your credentials in `index.js`. The order is:
+   - table-setup.sql
+   - menu-items.sql
+   - menu-descriptions.sql
+   - menu-prices.sql
+5. Execute the cmd `nodemon` to start the server.
--- index.js
+++ index.js
@@ -45,6 +45,98 @@ app.get("/menu/combo", async (req, res) => {
 	}
 });
 
+// GET endpoint for Main Dishes menu items
+app.get("/menu/main", async (req, res) => {
+	try {
+		const result = await pool.query(
+			`SELECT 
+          md.lang_code,
+          md.title,
+          md.description,
+          mp.price::TEXT,
+          mi.code
+        FROM menu_items mi
+        JOIN menu_descriptions md ON mi.id = md.item_id
+        JOIN menu_prices mp ON mi.id = mp.item_id
+        JOIN menu_categories mc ON mi.category_id = mc.id
+        WHERE mc.category_name = 'Main Dishes'`
+		);
+		res.json(result.rows);
+	} catch (err) {
+		console.error(err);
+		res.status(500).send("Error fetching menu items");
+	}
+});
+
+// GET endpoint for Soup & Noodles menu items
+app.get("/menu/soup&noodles", async (req, res) => {
+	try {
+		const result = await pool.query(
+			`SELECT 
+          md.lang_code,
+          md.title,
+          md.description,
+          mp.price::TEXT,
+          mi.code
+        FROM menu_items mi
+        JOIN menu_descriptions md ON mi.id = md.item_id
+        JOIN menu_prices mp ON mi.id = mp.item_id
+        JOIN menu_categories mc ON mi.category_id = mc.id
+        WHERE mc.category_name = 'Soup & Noodles'`
+		);
+		res.json(result.rows);
+	} catch (err) {
+		console.error(err);
+		res.status(500).send("Error fetching menu items");
+	}
+});
+
+// GET endpoint for Specials & Extras menu items
+app.get("/menu/specials&extras", async (req, res) => {
+	try {
+		const result = await pool.query(
+			`SELECT 
+          md.lang_code,
+          md.title,
+          md.description,
+          mp.price::TEXT,
+          mi.code
+        FROM menu_items mi
+        JOIN menu_descriptions md ON mi.id = md.item_id
+        JOIN menu_prices mp ON mi.id = mp.item_id
+        JOIN menu_categories mc ON mi.category_id = mc.id
+        WHERE mc.category_name = 'Specials & Extras'`
+		);
+		res.json(result.rows);
+	} catch (err) {
+		console.error(err);
+		res.status(500).send("Error fetching menu items");
+	}
+});
+
+// GET endpoint for Desserts & Drinks menu items
+app.get("/menu/desserts&drinks", async (req, res) => {
+	try {
+		const result = await pool.query(
+			`SELECT 
+          md.lang_code,
+          md.title,
+          md.description,
+          mp.price::TEXT,
+          mi.code
+        FROM menu_items mi
+        JOIN menu_descriptions md ON mi.id = md.item_id
+        JOIN menu_prices mp ON mi.id = mp.item_id
+        JOIN menu_categories mc ON mi.category_id = mc.id
+        WHERE mc.category_name = 'Desserts & Drinks'`
+		);
+		res.json(result.rows);
+	} catch (err) {
+		console.error(err);
+		res.status(500).send("Error fetching menu items");
+	}
+});
+
 app.listen(port, () => {
 	console.log(`Server running at http://localhost:${port}`);
 });
