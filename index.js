d5f1be978b9aafce23fd189483560ffca9511c2e
Dickson Lee <dicksonT_lee@hotmail.com>
2025-06-17 15:08:43 -0400

    update db port

--- index.js
+++ index.js
@@ -137,6 +137,6 @@ app.get("/menu/desserts&drinks", async (req, res) => {
 	}
 });
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
 	console.log(`Server running at http://localhost:${port}`);
 });
