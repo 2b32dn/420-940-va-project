c815aeee35bc38ecc0b4fe536fbb17dcb0fc719b
Dickson Lee <DicksonT_Lee@hotmail.com>
2025-06-19 03:57:31 -0400

    bugfix/MR-55: Revamp Header + clean some files (#35)

---
+++ js/header.js
@@ -0,0 +1,10 @@
+function initHeaderBehavior() {
+  const hamburgerBtn = document.getElementById("mrn-c-btn--hamburger");
+  const mobileMenu = document.querySelector(".mrn-c-nav__section.mrn-c-nav__section--right.mrn-c-nav__section--right-mobile");
+
+  if (hamburgerBtn && mobileMenu) {
+    hamburgerBtn.addEventListener("click", () => {
+      mobileMenu.classList.toggle("is-visible");
+    });
+  }
+}
