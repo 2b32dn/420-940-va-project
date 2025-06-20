94158a0d3e106f96b10b3d6ae6ec3969be3bb6a8
dicksonlee <dicksont_lee@hotmail.com>
2025-06-20 02:23:06 -0400

    style/MR-63: Style dropdown + fix js

--- js/header.js
+++ js/header.js
@@ -1,10 +1,26 @@
 function initHeaderBehavior() {
   const hamburgerBtn = document.getElementById("mrn-c-btn--hamburger");
   const mobileMenu = document.querySelector(".mrn-c-nav__section.mrn-c-nav__section--right.mrn-c-nav__section--right-mobile");
+  const body = document.body;
 
   if (hamburgerBtn && mobileMenu) {
     hamburgerBtn.addEventListener("click", () => {
       mobileMenu.classList.toggle("is-visible");
+      hamburgerBtn.classList.toggle("is-close")
     });
   }
 }
+
+// const navBtnHamburger = document.getElementById("nav-btn--hamburger");
+// const navMobile = document.getElementById("cv-nav--mobile");
+// const body = document.body;
+
+// navBtnHamburger.addEventListener("click", () => {
+//   navMobile.classList.toggle("is-hidden");
+//   navBtnHamburger.classList.toggle("cv-nav__btn--hamburger-close");
+//   if (!navMobile.classList.contains("is-hidden")) {
+//     body.classList.add("no-scroll");
+//   } else {
+//     body.classList.remove("no-scroll");
+//   }
+// });
